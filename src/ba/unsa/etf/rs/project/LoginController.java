41cf42205e200d2ef1b54a102346638bbd2a2100
HARUN <harun.hadzic17@gmail.com>
2019-06-19 21:28:45 +0200

    Implementacija LoginConroller klase->nepotpuno

---
+++ src/ba/unsa/etf/rs/project/LoginController.java
@@ -0,0 +1,64 @@
+package ba.unsa.etf.rs.project;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+
+public class LoginController {
+    public TextField nameField;
+    public PasswordField passwordField;
+
+    public void loginAction(ActionEvent actionEvent) {
+        boolean sveOk = true;
+        sveOk = isEmptyy(nameField);
+        sveOk &= isEmptyy(passwordField);
+
+        if (!sveOk) return;
+
+        if(Exists(nameField.getText(),passwordField.getText())){
+            Stage stage = new Stage();
+            Parent root = null;
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/main.fxml"));
+                MainController mainController = new MainController(null);
+                loader.setController(mainController);
+                root = loader.load();
+                stage.setTitle("Movie Library");
+                stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+                stage.setResizable(false);
+                stage.show();
+            } catch (IOException e) {
+            e.printStackTrace();
+        }
+        }
+        else return;
+    }
+    private boolean isEmptyy(TextField field) {
+        if (field.getText().trim().isEmpty()) {
+            field.getStyleClass().removeAll("poljeIspravno");
+            field.getStyleClass().add("poljeNijeIspravno");
+            return false;
+        } else {
+            field.getStyleClass().removeAll("poljeNijeIspravno");
+            field.getStyleClass().add("poljeIspravno");
+        }
+        return true;
+    }
+    private boolean Exists(String text , String pas) {
+        if(text == "administrator" &&  pas == "pass") return true;
+        return false;
+    }
+
+    public void cancleAction(ActionEvent actionEvent) {
+        Stage stage = (Stage) nameField.getScene().getWindow();
+        stage.close();
+    }
+}
