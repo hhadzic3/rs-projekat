1c2c8438afaaa6acefc7b1368ee90d0e1784b33e
HARUN <harun.hadzic17@gmail.com>
2019-06-22 18:31:19 +0200

    Implementacija Administrator klase

---
+++ src/ba/unsa/etf/rs/project/Administrator.java
@@ -0,0 +1,30 @@
+package ba.unsa.etf.rs.project;
+
+public class Administrator {
+    private String username , password;
+
+    public Administrator() {
+    }
+
+    public Administrator(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
--- src/ba/unsa/etf/rs/project/LoginController.java
+++ src/ba/unsa/etf/rs/project/LoginController.java
@@ -1,9 +1,11 @@
 package ba.unsa.etf.rs.project;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -19,6 +21,13 @@ public class LoginController {
     public TextField nameField;
     public PasswordField passwordField;
 
+    private Administrator user;
+    private AdministratorDAO userDAO;
+
+    @FXML
+    private void initialize(){
+        this.userDAO = new AdministratorDAO();
+    }
 
     public void loginAction(ActionEvent actionEvent) {
         boolean sveOk = true;
@@ -27,26 +36,55 @@ public class LoginController {
 
         if (!sveOk) return;
 
-        if(Exists(nameField.getText(),passwordField.getText())){
-            Stage stage = new Stage();
-            try {
-                MovieLibraryDAO model = MovieLibraryDAO.getInstance();
-                FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/mainform.fxml"));
-                MainController mainController = new MainController(model);
-                loader.setController(mainController);
-                Parent root = loader.load();
-                stage.setTitle("Movie Library");
-                stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
-                stage.show();
-            } catch (IOException e) {
-            e.printStackTrace();
-            }
-            finally {
-                Stage s = (Stage) nameField.getScene().getWindow();
-                s.close();
-            }
+        String username = this.nameField.getText();
+        String password = this.nameField.getText();
+
+        if(username.trim().equals("") || password.trim().equals("")){
+            this.nameField.setPromptText("Invalid credentials");
+            this.passwordField.setPromptText("Invalid credentials");
+            return;
         }
-        else return;
+
+        AdministratorDAO userDAO = new AdministratorDAO();
+
+        this.user = userDAO.findUser(username);
+
+        if (this.user == null){
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Warning");
+            alert.setHeaderText("User does not exist!");
+            alert.setContentText("User does not exist!" );
+
+        } else {
+            if(this.user.getPassword().equals(password)){
+                Stage stage = new Stage();
+                try {
+                    MovieLibraryDAO model = MovieLibraryDAO.getInstance();
+                    FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/mainform.fxml"));
+                    MainController mainController = new MainController(model);
+                    loader.setController(mainController);
+                    Parent root = loader.load();
+                    stage.setTitle("Movie Library");
+                    stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+                    stage.show();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                finally {
+                    Stage s = (Stage) nameField.getScene().getWindow();
+                    s.close();
+                }
+            }
+
+            else {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Warning");
+            alert.setHeaderText("User password does not exist!");
+            alert.setContentText("User password does not exist!" );
+            }
+
+        }
+
     }
     private boolean isEmptyy(TextField field) {
         if (field.getText().trim().isEmpty()) {
@@ -59,13 +97,21 @@ public class LoginController {
         }
         return true;
     }
-    private boolean Exists(String text , String pas) {
-        if(text.equals("admin") &&  pas.equals("pass")) return true;
-        return false;
-    }
+
 
     public void cancleAction(ActionEvent actionEvent) {
         Stage stage = (Stage) nameField.getScene().getWindow();
         stage.close();
     }
+
+    @FXML
+    private void register() throws IOException {
+        Stage stage = null;
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/register.fxml"));
+        stage.setTitle("Register");
+        stage.setScene(new Scene(root, 300, 275));
+        stage.show();
+    }
+
+
 }
