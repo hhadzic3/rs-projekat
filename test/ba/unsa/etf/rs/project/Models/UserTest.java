bf68039ecbdcda1ff8cb8a62bc9b9e202b3065d7
HARUN <harun.hadzic17@gmail.com>
2019-06-28 18:49:50 +0200

    Implementacija testne klase User

--- test/ba/unsa/etf/rs/project/Models/UserTest.java
+++ test/ba/unsa/etf/rs/project/Models/UserTest.java
@@ -1,7 +1,41 @@
 package ba.unsa.etf.rs.project.Models;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserTest {
+
+    @Test
+    void getName() {
+        User k = new User("a","b");
+        assertEquals("a", k.getName());
+    }
+
+    @Test
+    void getPassword() {
+        User k = new User("a","b");
+        assertEquals("b", k.getSurname());
+    }
+
+    @Test
+    void setName() {
+        User k = new User("a","b");
+        assertEquals("a", k.getName());
+    }
+    @Test
+    void setPassword() {
+        User k = new User("a","b");
+        assertEquals("b", k.getSurname());
+    }
+
+
+    @Test
+    void toStringTest() {
+        User k = new User("a","b");
+        String result = "" + k;
+        String expected = "a b";
+        assertEquals(expected, result);
+    }
 
 }
